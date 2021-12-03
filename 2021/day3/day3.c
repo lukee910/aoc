e8d5dd6e4a3a4a87087c444f8a6d84e1e19b9ded
Lukas Doerig <ldoerig@student.ethz.ch>
2021-12-03 21:28:21 +0100

    same, same

--- 2021/day3/day3.c
+++ 2021/day3/day3.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define DEBUG
 
@@ -16,9 +17,9 @@
 #define NUM_MEASURES 1000
 #define WIDTH_MEASURES 12
 
-int **task1(int print);
+void task1(int print);
 void task2();
-int findPattern(int pattern, int *in);
+int findval(int in[NUM_MEASURES][WIDTH_MEASURES], int takemaj);
 void printBinary(int value);
 
 int main()
@@ -29,7 +30,7 @@ int main()
   return 0;
 }
 
-int **task1(int print)
+void task1(int print)
 {
   FILE *input = fopen("day3/input.txt", "r");
 
@@ -85,80 +86,77 @@ int **task1(int print)
     printBinary(eps);
     printf(", Product: %d\n", gamma * eps);
   }
-
-  int *values = malloc(2 * sizeof(int));
-  values[0] = gamma;
-  values[1] = eps;
-
-  int **ret = malloc(2 * sizeof(int));
-  ret[0] = values;
-  ret[1] = in;
-
-  return ret;
 }
 
 void task2()
 {
-  int **t1res = task1(0);
+  FILE *input = fopen("day3/input.txt", "r");
 
-  int gamma = t1res[0][0];
-  int eps = t1res[0][1];
-  int *in = t1res[1];
-
-  // Gamma is the expected bit pattern for O2, eps is the expected bit pattern for CO2
-  int o2 = findPattern(gamma, in);
-  int co2 = findPattern(eps, in);
-
-  printf("O2: %d, CO2: %d, Product: %d\n", o2, co2, o2 * co2);
-}
-
-int findPattern(int pattern, int *in)
-{
-  int count = NUM_MEASURES;
-  int col = WIDTH_MEASURES - 1;
-
-#ifdef DEBUG
-  printf("Finding pattern: ");
-  printBinary(pattern);
-  printf("\n");
-#endif
-
-  int nextrow = 0;
-  int curr[NUM_MEASURES];
+  // Screw optimization in an int like in task 1
+  int in[NUM_MEASURES][WIDTH_MEASURES];
 
   for (int i = 0; i < NUM_MEASURES; i++)
   {
-    curr[i] = in[i];
+    // Init value in array
+    for (int j = 0; j < WIDTH_MEASURES; j++)
+    {
+      in[i][j] = getc(input) - '0';
+    }
+
+    // Read newline/EOF
+    getc(input);
   }
 
+  int o2 = findval(in, 1);
+  int co2 = findval(in, 0);
+
+  printf("o2: %d, co2: %d, product: %d\n", o2, co2, o2 * co2);
+}
+
+int findval(int in[NUM_MEASURES][WIDTH_MEASURES], int takemaj)
+{
+  int values[NUM_MEASURES][WIDTH_MEASURES];
+  memcpy(values, in, NUM_MEASURES * WIDTH_MEASURES * sizeof(int));
+
+  int count = NUM_MEASURES;
+  int col = WIDTH_MEASURES - 1;
+  int next = 0;
   while (count > 1)
   {
-    DPRINT("-- Iteration start\n");
-    DPRINT("col  : %d\n", col);
-    DPRINT("count: %d\n", count);
-    // DPRINT("curr : %p\n", curr);
+    DPRINT("-- start iteration --\n");
+    int num1 = 0;
+    for (int i = 0; i < count; i++)
+    {
+      if (values[i][col])
+      {
+        num1++;
+      }
+    }
+    int take1 = num1 >= (count / 2);
+    if (!takemaj)
+    {
+      take1 = !take1;
+    }
+    DPRINT("count: %d, num1: %d, take1: %d\n", count, num1, take1);
 
     for (int i = 0; i < count; i++)
     {
-      if (((curr[i] >> col) & 1) == ((pattern >> col) & 1))
+      if (values[i][col] == take1)
       {
-        curr[nextrow++] = curr[i];
-
-        if (count < 9)
-        {
-          DPRINT("Entry added: %X, index %d to %d\n", curr[i], i, nextrow - 1);
-        }
+        memcpy(values[next++], values[i], WIDTH_MEASURES * sizeof(int));
       }
     }
 
-    count = nextrow - 1;
-    nextrow = 0;
-    col--;
+    count = next - 1;
+    next = 0;
+  }
+  int result = 0;
+  for (int i = 0; i < WIDTH_MEASURES; i++)
+  {
+    result = (result << 1) | values[0][i];
   }
 
-  DPRINT("Pattern %X matched with %X\n", pattern, curr[0]);
-
-  return curr[0];
+  return result;
 }
 
 void printBinary(int value)
